3bc59587709b7bc444a490864a57a3a097e664a3
shikhartuli <shikhartuli98@gmail.com>
2022-03-10 23:58:58 -0500

    Added GPU support for macOS

--- energy_profiler/tests/test_power.py
+++ energy_profiler/tests/test_power.py
@@ -15,10 +15,20 @@ import multiprocessing as mp
 from matplotlib import pyplot as plt
 
 from run_glue import main as run_glue
+import torch
+
+if platform.system() == 'Darwin':
+	# Running tensorflow version for macOS on arm64
+	from run_glue_tf import main as run_glue_tf
+	import tensorflow as tf
+	
+
+sys.path.append('../../txf_design-space/transformers/src/transformers')
 
 
 OUTPUT_DIR = './bert_tiny_sst2'
-RUNS = 10
+RUNS = 3
+USE_GPU = True
 
 
 def get_training_args(seed, output_dir):
@@ -76,21 +86,41 @@ def get_power(debug: bool = False):
 	return {'cpu': cpu_power, 'gpu': gpu_power, 'dram': dram_power}
 
 
-def run_bert_inference(queue, runs: int, output_dir: str):
+def run_bert_inference(queue, gpu: bool, runs: int, output_dir: str):
 	"""Run inference of BERT-Tiny on the SST-2 task
 	
 	Args:
 	    queue (mp.Queue): multiprocessing queue
-	    runs (int, optional): number of inference runs
+	    gpu (bool): to run on GPU or not
+	    runs (int): number of inference runs
 	    output_dir (str): directory where the pre-trained model is stored
 	
 	Returns:
 	    dict: evaluation metrics
 	"""
+	start_time = time.time()
 	for i in range(runs):
-		run_glue(get_training_args(0, output_dir))
+		if gpu:
+			if platform.system() == 'Darwin':
+				run_glue_tf(get_training_args(0, output_dir))
+			else:
+				run_glue(get_training_args(0, output_dir))
+		else:
+			if platform.system() == 'Darwin':
+				run_glue(get_training_args(0, output_dir))
+			else:
+				os.environ['CUDA_VISIBLE_DEVICES'] = ''
+				run_glue(get_training_args(0, output_dir))
 
-	eval_metrics = json.load(open(os.path.join(output_dir, 'eval_results.json'), 'r'))
+	end_time = time.time()
+
+	if platform.system() == 'Darwin':
+		with open(os.path.join(output_dir, 'eval_results.txt')) as file:
+			lines = file.readlines()
+		eval_metrics = {'eval_loss': float(lines[0].split(' ')[-1]), 'eval_accuracy': float(lines[1].split(' ')[-1]), 'eval_runtime': end_time - start_time}
+	else:
+		eval_metrics = json.load(open(os.path.join(output_dir, 'eval_results.json'), 'r'))
+
 	queue.put(eval_metrics)
 
 
@@ -99,7 +129,7 @@ def main():
 	bert_queue = mp.Queue()
 
 	# Get process
-	bert_process = mp.Process(target=run_bert_inference, args=(bert_queue, RUNS, OUTPUT_DIR))
+	bert_process = mp.Process(target=run_bert_inference, args=(bert_queue, USE_GPU, RUNS, OUTPUT_DIR))
 
 	start_time = time.time()
 	power_metrics = []
@@ -112,7 +142,7 @@ def main():
 	bert_process.start()
 
 	# Get power consumption for 10 more iterations
-	for i in range(15):
+	for i in range(25):
 		power_metrics.append({'power_metrics': get_power(debug=True), 'time': time.time() - start_time})
 
 	eval_metrics = bert_queue.get()
@@ -124,17 +154,30 @@ def main():
 		power_metrics[i]['time'] -= exp_start_time
 
 	# Make a plot of all power metrics
-	plt.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['cpu'] for meas in power_metrics], label='CPU Power', color='b')
-	plt.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['gpu'] for meas in power_metrics], label='GPU Power', color='g')
-	plt.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['dram'] for meas in power_metrics], label='DRAM Power', color='r')
-	plt.axvline(x=power_metrics[4]['time'], linestyle='--', color='k')
-	plt.axvline(x=power_metrics[4]['time']+eval_metrics['eval_runtime']*RUNS, linestyle='--', color='k')
-	plt.xlabel('Time (s)')
-	plt.ylabel('Power (mW)')
-	plt.legend()
-	plt.savefig('./results/power_results.pdf')
+	fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+	ax1.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['cpu']/1000.0 for meas in power_metrics], label='CPU Power', color='b')
+	ax2.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['gpu'] for meas in power_metrics], label='GPU Power', color='g')
+	ax3.plot([meas['time'] for meas in power_metrics], [meas['power_metrics']['dram']/1000.0 for meas in power_metrics], label='DRAM Power', color='r')
 
-	json.dump(power_metrics, open('./results/power_metrics.json', 'w+'))
+	ax1.axvline(x=power_metrics[4]['time'], linestyle='--', color='k')
+	ax1.axvline(x=power_metrics[4]['time']+eval_metrics['eval_runtime'], linestyle='--', color='k')
+	ax2.axvline(x=power_metrics[4]['time'], linestyle='--', color='k')
+	ax2.axvline(x=power_metrics[4]['time']+eval_metrics['eval_runtime'], linestyle='--', color='k')
+	ax3.axvline(x=power_metrics[4]['time'], linestyle='--', color='k')
+	ax3.axvline(x=power_metrics[4]['time']+eval_metrics['eval_runtime'], linestyle='--', color='k')
+
+	ax1.set_xlabel('Time (s)')
+	ax2.set_xlabel('Time (s)')
+	ax3.set_xlabel('Time (s)')
+
+	ax1.set_ylabel('CPU Power (W)')
+	ax2.set_ylabel('GPU Power (mW)')
+	ax3.set_ylabel('DRAM Power (W)')
+
+	ax1.set_title(f'Model: BERT-Tiny | Task: SST-2 | Runtime: {eval_metrics["eval_runtime"] : 0.2f}s for {RUNS} runs')
+	plt.savefig(os.path.join(OUTPUT_DIR, 'power_results.pdf'))
+
+	json.dump(power_metrics, open(os.path.join(OUTPUT_DIR, 'power_metrics.json'), 'w+'))
 
 	print(f'Evaluation Accuracy (%): {eval_metrics["eval_accuracy"]*100}. Evaluation Runtime (s): {eval_metrics["eval_runtime"]}')
 
